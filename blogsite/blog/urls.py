aa5ba27ce9dd68250d5669c2fd30b81228b6489a
Bernardo Cardoso <bcstark21@gmail.com>
2025-07-27 18:50:52 +0100

    #2: splitted into different components core and articles

--- blogsite/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('articles/', views.articles_list, name='articles'),
-    path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
-]
--- my_blog/articles/views.py
+++ my_blog/articles/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 
-def home(request):
-    return render(request, 'blog/home.html')
 
 def articles_list(request):
     articles = Article.objects.all().order_by('-published_at')
-    return render(request, 'blog/articles.html', {'articles': articles})
+    return render(request, 'articles.html', {'articles': articles})
 
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, 'blog/article_detail.html', {'article': article})
+    return render(request, 'article_detail.html', {'article': article})
